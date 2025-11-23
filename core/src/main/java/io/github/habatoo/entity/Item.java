bea04489b98211cacbbabe9a8bdc9de3f3802829
Stanislav Laptenkov <habatoo@yandex.ru>
2025-11-23 22:42:42 +0700

    #Entity commit
    Для перевода entity на реактивный стек:
    - исправлены entity.

--- core/src/main/java/io/github/habatoo/entity/Item.java
+++ core/src/main/java/io/github/habatoo/entity/Item.java
@@ -1,103 +1,36 @@
 package io.github.habatoo.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
 /**
  * Товар в интернет-магазине.
  * Хранит название, описание, изображение, цену и вспомогательное поле для отображения количества.
  */
-@Entity
+@Table("items")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "items")
 public class Item {
-    /** Идентификатор товара (PK). */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /** Название товара. */
-    @Column(nullable = false)
     private String title;
 
-    /** Описание товара. */
-    @Column(length = 1024)
     private String description;
 
-    /** Путь к файлу изображения товара. */
-    @Column
+    @Column("img_path")
     private String imgPath;
 
-    /** Цена товара. */
-    @Column(nullable = false)
     private BigDecimal price;
 
-    /** Текущее количество для отображения (не хранится в БД). */
     @Transient
     private Integer count = 0;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImgPath() {
-        return imgPath;
-    }
-
-    public void setImgPath(String imgPath) {
-        this.imgPath = imgPath;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return Objects.equals(id, item.id) && Objects.equals(title, item.title) && Objects.equals(description, item.description) && Objects.equals(imgPath, item.imgPath) && Objects.equals(price, item.price) && Objects.equals(count, item.count);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, description, imgPath, price, count);
-    }
 }
--- core/src/main/java/io/github/habatoo/entity/Order.java
+++ core/src/main/java/io/github/habatoo/entity/Order.java
@@ -1,69 +1,30 @@
 package io.github.habatoo.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Заказ пользователя в интернет-магазине.
  * Хранит идентификатор, список заказанных товаров, итоговую сумму заказа и дату оформления.
  */
-@Entity
+@Table("orders")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "orders")
 public class Order {
-    /** Идентификатор заказа (PK). */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /** Список позиций в заказе. */
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<OrderItem> items = new ArrayList<>();
-
-    /** Итоговая сумма заказа. */
-    @Column(nullable = false)
+    @Column("total_sum")
     private BigDecimal totalSum;
 
-    /** Дата и время оформления заказа. */
-    @Column(nullable = false)
+    @Column("date_time")
     private LocalDateTime dateTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<OrderItem> getItems() {
-        return items;
-    }
-
-    public void setItems(List<OrderItem> items) {
-        this.items = items;
-    }
-
-    public BigDecimal getTotalSum() {
-        return totalSum;
-    }
-
-    public void setTotalSum(BigDecimal totalSum) {
-        this.totalSum = totalSum;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
 }
